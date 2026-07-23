d77044d7a9832a9437b97790155cdec2389f0e01
Abdullobek <43066163+norboyev1990@users.noreply.github.com>
2026-07-23 18:43:57 +0500

    Update ALLOWED_HOSTS for production settings

--- smart_storage/settings.py
+++ smart_storage/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@5ghdemmvi&)eoko5)+z99lo2((d6crrzz+^4!a933tqrernzd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['smartec.uz']
 
 
 # Application definition
