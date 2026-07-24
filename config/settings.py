3eee48510d060f124af52c72479b49c7d4b4d2a4
Abdulla <norboyev1990@gmail.com>
2026-07-24 14:30:37 +0500

    fix config

--- config/settings.py
+++ config/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,11 +23,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@5ghdemmvi&)eoko5)+z99lo2((d6crrzz+^4!a933tqrernzd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
-    'smartec.uz'
+    'localhost',
+    'smartec.uz',
+    'app.smartec.uz',
+    'www.smartec.uz',
 ]
 
 
@@ -121,10 +124,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+# STATIC_URL = 'static/'
 
-MEDIA_URL = 'media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+STATIC_ROOT = os.path.join(BASE_DIR, 'public/static')
+STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'public/media')
 
 AUTH_USER_MODEL = 'users.TelegramUser'
 
--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,9 +1,8 @@
-import imp
 import os
 import sys
 
-
 sys.path.insert(0, os.path.dirname(__file__))
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
-wsgi = imp.load_source('wsgi', 'config/wsgi.py')
-application = wsgi.application
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
